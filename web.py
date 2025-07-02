e79e303d400c106464089a767468f53383853a47
Matthew Kobayashi <1856537+MattKobayashi@users.noreply.github.com>
2025-07-02 10:35:46 +1000

    fix: add integrity attribute to external scripts and CSS (#86)

--- web.py
+++ web.py
@@ -90,7 +90,7 @@ def read_form():
     <head>
         <meta charset="UTF-8">
         <title>Generate Cover Letter</title>
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous">
         <script>
         function disableSubmit() {
             var btn = document.getElementById("submitBtn");
@@ -186,7 +186,7 @@ Focus on matching key skills and experience. Use professional tone. Write in {la
         <head>
             <meta charset="UTF-8">
             <title>Generated Cover Letter</title>
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous">
         </head>
         <body>
         <div class="container mt-5">
@@ -214,7 +214,7 @@ Focus on matching key skills and experience. Use professional tone. Write in {la
         <head>
             <meta charset="UTF-8">
             <title>Error</title>
-            <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet">
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha512-fw7f+TcMjTb7bpbLJZlP8g2Y4XcCyFZW8uy8HsRZsH/SwbMw0plKHFHr99DN3l04VsYNwvzicUX/6qurvIxbxw==" crossorigin="anonymous">
         </head>
         <body>
         <div class="container mt-5">
